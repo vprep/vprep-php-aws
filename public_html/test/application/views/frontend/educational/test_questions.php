928b1f87a3db400183aee308c099853a6d59567b
SwadhinRaman <swadhinraman@SwadhinRamans-MacBook-Air.local>
2018-05-25 15:08:40 +0530

    pushing my changes

--- public_html/test/application/views/frontend/educational/test_questions.php
+++ public_html/test/application/views/frontend/educational/test_questions.php
@@ -485,6 +485,13 @@ if (!empty($test_ques) && !empty($test_ques['sub_module'])) {
                                             </div>
                                         </div>
                                     </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="box">
+            <div class="box-body text-right add_question_tab">
+                <?php echo anchor('add_test_questions_passage','<i class="fa fa-plus-square"></i> &nbsp;Add Question passage',array("class"=>"btn btn-primary btn-lg")); ?>
+            </div>
+        </div>
+    </div>
                                 </div>
 
 
