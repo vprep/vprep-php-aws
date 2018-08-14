c6927258645bd07754a1d3d6877e9251eb6c4bca
swadhin <swadhin@vprep.in>
2018-08-14 23:20:38 +0530

    adding essaywriting

--- public_html/test/application/controllers/Student.php
+++ public_html/test/application/controllers/Student.php
@@ -229,7 +229,7 @@ class Student extends CI_Controller {
         $data["topic"]=$this->db->query("select topic from essayWriting where id=$essayId")->result_array();
 
 
-        echo $data["topic"];
+        echo $data["topic"][0];
         $this->template->load('frontend','frontend/student/essayWrite',$data);
 
     }
