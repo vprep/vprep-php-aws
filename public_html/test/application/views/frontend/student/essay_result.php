a5551b1639a3fecc1e8fff178d2248f8c2258ba7
swadhin <swadhin@vprep.in>
2018-08-15 18:55:06 +0530

    adding essaywriting

--- public_html/test/application/views/frontend/student/essay_result.php
+++ public_html/test/application/views/frontend/student/essay_result.php
@@ -18,8 +18,12 @@ Answer: <?php echo $topic['answer']; ?>
     var json1=<?php echo $topic['evaluation']; ?>
 
     for(var i=0;json1.check_grammar_feedback.length;i++){
+        var div = document.createElement('div');
+
         var html1=json1.check_grammar_feedback[i]["context"]+" "+json1.check_grammar_feedback[i]["error_grammar"]+" "+json1.check_grammar_feedback[i]["feedback_grammar"]+json1.check_grammar_feedback[i]["feedback_grammar_suggestion"];
-        alert(html1);
+        div.innerHTML=html1;
+       var  feedback1 =document.getElementsByClassName("feedback");
+        feedback1.appendChild(div);
 
     }
 
