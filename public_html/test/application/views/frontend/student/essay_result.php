1cfefb26b813abaf22c5afc16797f29edb1d326b
swadhin <swadhin@vprep.in>
2018-08-15 18:03:20 +0530

    adding essaywriting

--- public_html/test/application/views/frontend/student/essay_result.php
+++ public_html/test/application/views/frontend/student/essay_result.php
@@ -5,3 +5,12 @@ echo $topic['user_id'];
 echo $topic['answer'];
 
 ?>
+
+
+topic:
+Answer: <?php echo $topic['answer']; ?>
+<script>
+
+    var json1=<?php echo $topic['evaluation']; ?>
+
+    </script>
