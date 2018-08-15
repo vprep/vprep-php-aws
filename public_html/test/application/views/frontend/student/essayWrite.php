63bb1820a37bcd92fe8b6e9c53638e21149e3433
swadhin <swadhin@vprep.in>
2018-08-15 12:32:24 +0530

    adding essaywriting

--- public_html/test/application/views/frontend/student/essayWrite.php
+++ public_html/test/application/views/frontend/student/essayWrite.php
@@ -7,8 +7,27 @@ echo $topic['topic'];
 
 <form action="/action_page.php">
 
-    Write here:<br>
-    <input type="text" name="lastname">
+    <div style="
+    padding-bottom: 1px;
+    padding-left: 10%;
+    font-size: 19px;
+    font-weight: bold;
+    color: lightblue;
+"> Write here:</div><br>
+    <input align="top" type="text" name="lastname" style="
+    width: 80%;
+    height: 500px;
+    margin-left: 10%;
+    align: top;
+    padding-top: 250px;
+">
     <br><br>
-    <input type="submit">
+    <input type="submit" style="
+    text-align: center;
+    margin-left: 46%;
+    margin-bottom: 10px;
+    width: 7%;
+    background: green;
+    color: beige;
+">
 </form>
