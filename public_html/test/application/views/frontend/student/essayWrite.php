c9634fa4ac4c790eacfc48c4687d7249879cea08
swadhin <swadhin@vprep.in>
2018-08-15 11:47:45 +0530

    adding essaywriting

--- public_html/test/application/views/frontend/student/essayWrite.php
+++ public_html/test/application/views/frontend/student/essayWrite.php
@@ -2,4 +2,13 @@ this is good.
 
 <?php
 echo $topic['topic'];
+
 ?>
+
+<form action="/action_page.php">
+
+    Write here:<br>
+    <input type="text" name="lastname">
+    <br><br>
+    <input type="submit">
+</form>
