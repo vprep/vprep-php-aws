71e4af91e7ad69e571d009273e19a2037e0cd4de
swadhin <swadhin@vprep.in>
2018-08-15 18:37:10 +0530

    adding essaywriting

--- public_html/test/application/views/frontend/student/essay_result.php
+++ public_html/test/application/views/frontend/student/essay_result.php
@@ -9,8 +9,18 @@ echo $topic['answer'];
 
 topic:
 Answer: <?php echo $topic['answer']; ?>
+
+<div class="feedback">
+
+</div>>
 <script>
 
     var json1=<?php echo $topic['evaluation']; ?>
+
+    for(var i=0;json1.check_grammar_feedback.length;i++){
+        var html1=json1.check_grammar_feedback[i]["context"]+" "+json1.check_grammar_feedback[i]["error_grammar"]+" "+json1.check_grammar_feedback[i]["feedback_grammar"]+json1.check_grammar_feedback[i]["feedback_grammar_suggestion"];
+        alert(html1);
+
+    }
 
     </script>
