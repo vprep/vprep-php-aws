889fc276dab1620ba1eb639acef6a5ba7b4d66ff
swadhin <swadhin@vprep.in>
2018-08-15 19:11:44 +0530

    adding essaywriting

--- public_html/test/application/views/frontend/student/essay_result.php
+++ public_html/test/application/views/frontend/student/essay_result.php
@@ -20,7 +20,7 @@ Answer: <?php echo $topic['answer']; ?>
     for(var i=0;json1.check_grammar_feedback.length;i++){
         var div = document.createElement('div');
 
-        var html1="<div>"+json1.check_grammar_feedback[i]["context"]+"</div><div> "+json1.check_grammar_feedback[i]["error_grammar"]+"</div><div>"+json1.check_grammar_feedback[i]["feedback_grammar"]+"</div><div>"+json1.check_grammar_feedback[i]["feedback_grammar_suggestion"]+"</div>";
+        var html1="<div>Sentence:"+json1.check_grammar_feedback[i]["context"]+"</div><div> Error Place:"+json1.check_grammar_feedback[i]["error_grammar"]+"</div><div>Feedback Grammer:"+json1.check_grammar_feedback[i]["feedback_grammar"]+"</div><div>Grammer Suggestion "+json1.check_grammar_feedback[i]["feedback_grammar_suggestion"]+"</div>";
         div.innerHTML=html1;
        var  feedback1 =document.getElementsByClassName("feedback");
         feedback1[0].appendChild(div);
