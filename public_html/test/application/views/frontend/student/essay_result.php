9c5e399ddb7ced9badd6f4ae4b1e8d6f36b760c9
swadhin <swadhin@vprep.in>
2018-08-16 12:17:04 +0530

    adding essaywriting

--- public_html/test/application/views/frontend/student/essay_result.php
+++ public_html/test/application/views/frontend/student/essay_result.php
@@ -8,16 +8,23 @@ topic:
 </div>
 <script>
 
-    var json1=<?php echo $topic['evaluation']; ?>
+    if(<?php echo $topic['generated']; ?>==1)
+    {
+        var json1 =<?php echo $topic['evaluation']; ?>
 
-    for(var i=0;json1.check_grammar_feedback.length;i++){
-        var div = document.createElement('div');
+        for (var i = 0; json1.check_grammar_feedback.length; i++) {
+            var div = document.createElement('div');
 
-        var html1="<br><span style='font-size:20px;'>"+i+":</span> "+"<div style='color: green;font-style: italic;'><b>Sentence: </b>"+json1.check_grammar_feedback[i]["context"]+"</div><div style='color:red; font-size:18px;font-weight: bold;'><b> Error Place: </b>"+json1.check_grammar_feedback[i]["error_grammar"]+"</div><div style='font-size: 18px;'><b>Feedback Grammer:</b>"+json1.check_grammar_feedback[i]["feedback_grammar"]+"</div> ";
-        div.innerHTML=html1;
-       var  feedback1 =document.getElementsByClassName("feedback");
-        feedback1[0].appendChild(div);
+            var html1 = "<br><span style='font-size:20px;'>" + i + ":</span> " + "<div style='color: green;font-style: italic;'><b>Sentence: </b>" + json1.check_grammar_feedback[i]["context"] + "</div><div style='color:red; font-size:18px;font-weight: bold;'><b> Error Place: </b>" + json1.check_grammar_feedback[i]["error_grammar"] + "</div><div style='font-size: 18px;'><b>Feedback Grammer:</b>" + json1.check_grammar_feedback[i]["feedback_grammar"] + "</div> ";
+            div.innerHTML = html1;
+            var feedback1 = document.getElementsByClassName("feedback");
+            feedback1[0].appendChild(div);
 
+        }
+    }
+    else {
+
+        alert("not generated");
     }
 
     </script>
