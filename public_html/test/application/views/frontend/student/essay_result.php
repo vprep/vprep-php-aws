6e0c8a4eea4e1ff3c0c297a91e96bb64e1c108b3
swadhin <swadhin@vprep.in>
2018-08-16 08:37:27 +0530

    adding essaywriting

--- public_html/test/application/views/frontend/student/essay_result.php
+++ public_html/test/application/views/frontend/student/essay_result.php
@@ -14,7 +14,7 @@ Answer: <?php echo $topic['answer']; ?>
     padding-left: 5%;
 ">
 
-</div>>
+</div>
 <script>
 
     var json1=<?php echo $topic['evaluation']; ?>
@@ -22,7 +22,7 @@ Answer: <?php echo $topic['answer']; ?>
     for(var i=0;json1.check_grammar_feedback.length;i++){
         var div = document.createElement('div');
 
-        var html1="<br>"+i+": "+"<div><b>Sentence:</b>"+json1.check_grammar_feedback[i]["context"]+"</div><div><b> Error Place:</b>"+json1.check_grammar_feedback[i]["error_grammar"]+"</div><div><b>Feedback Grammer:</b>"+json1.check_grammar_feedback[i]["feedback_grammar"]+"</div><div><b>Grammer Suggestion</b> "+json1.check_grammar_feedback[i]["feedback_grammar_suggestion"]+"</div>";
+        var html1="<br>"+i+": "+"<div style='color: green;font-style: italic;'><b>Sentence:</b>"+json1.check_grammar_feedback[i]["context"]+"</div><div style='color:red'><b> Error Place:</b>"+json1.check_grammar_feedback[i]["error_grammar"]+"</div><div><b>Feedback Grammer:</b>"+json1.check_grammar_feedback[i]["feedback_grammar"]+"</div><div><b>Grammer Suggestion</b> "+json1.check_grammar_feedback[i]["feedback_grammar_suggestion"]+"</div>";
         div.innerHTML=html1;
        var  feedback1 =document.getElementsByClassName("feedback");
         feedback1[0].appendChild(div);
