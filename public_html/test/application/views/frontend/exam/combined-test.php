8ba6581ebf103745c3864181890c185c59d410df
Dinesh Sharma <dsharma@upsnap.com>
2018-08-18 00:04:28 +0530

    essay changes

--- public_html/test/application/views/frontend/exam/combined-test.php
+++ public_html/test/application/views/frontend/exam/combined-test.php
@@ -64,7 +64,7 @@
                         jsonObj_71 = JSON.parse(data);
 
                         $.each(jsonObj_71, function (key,value) {
-                            if(parseInt(value["time"])<1400&&resumeTime_71<1400){
+                            if(parseInt(value["time"])<4400&&resumeTime_71<4400){
                                 resumeTime_71+=parseInt(value["time"]);
                             }
                         });
@@ -80,11 +80,11 @@
                         url: "http://test.vprep.in/read_exam_data?exam_id="+<?php echo $exam_1;?>,
                         method: "GET",
                         success: function(data) {
-                            min=1;
+                            min=80;
 
                             if(resumeTime_71 != null && resumeTime_71 >0 ){
 
-                                min = (25 - Math.floor(resumeTime_71/60));
+                                min = (80 - Math.floor(resumeTime_71/60));
 
                             }
                             sec = 0;
@@ -383,7 +383,7 @@
                                 //$(".prev-btn-2").show();
                                 $('.div-72-1').data("start_time",Math.round(new Date()/1000));
                                 activeExam2();
-                                min = 35;
+                                min = 80;
                                 sec = 0;
 
                                 var includeUrl_1 = "http://test.vprep.in/save_exam_json?exam_id="+<?php echo $exam_1;?>+"&ans_json="+JSON.stringify(jsonObj_71)+"&score="+score+"&start_at="+startTime+"&taken_status=2&save_status=71";
@@ -662,7 +662,7 @@
 
 <script type="text/javascript">
     var hour =0;
-    var min =1;
+    var min =80;
 
     var sec =0;
 
